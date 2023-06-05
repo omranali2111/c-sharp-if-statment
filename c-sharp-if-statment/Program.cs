87f8dd6335ec7502c0f40780179395a6b5ca2b0d
omranali2111 <aljabri1234@hotmail.com>
2023-06-05 09:53:10 +0400

    if statment

---
+++ c-sharp-if-statment/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // if statment 
+        //Less than: a < b
+        //Less than: a < b
+        //Less than or equal to: a <= b
+        //Greater than: a > b
+        //Greater than or equal to: a >= b
+        //Equal to a == b
+        //Not Equal to: a != b
+
+        int a = 0, b = 1;
+            bool c;
+        if(a == 0)
+        {
+            WriteLine("condition met");
+        }
+
+        else if(a == 1)
+        {
+            WriteLine("condition not met");
+        }
+        else
+        {
+            WriteLine("na");
+        }
+
+        
+        
+
+    }
+}
